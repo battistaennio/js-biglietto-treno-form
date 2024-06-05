9d48032b146200a100c5188e2f0ac049af5d3e7b
battistaennio <ennio.battista99@gmail.com>
2024-06-05 15:53:55 +0200

    Integrata logica per calcolo prezzo biglietto

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-// let risultato;
+let risultato;
 
 //selezione età
-let userAge = document.getElementById("age");
+let inputAge = document.getElementById("age");
 
 //selezione km
-let userKM = document.getElementById("km");
+let inputKM = document.getElementById("km");
 
 //selezione bottone
 const bottoneInvia = document.getElementById("collect_data")
 
 //evento click
 bottoneInvia.addEventListener("click",
-    function() {   
-
-        let valoreAge = parseInt(userAge.value);
+    function() {    
+        
+        //colleziono age e km
+        let valoreAge = parseInt(inputAge.value);
         console.log("valoreAge: ", valoreAge);
-                                                 //colleziono age e km
-        let valoreKM = parseInt(userKM.value);
+
+        let valoreKM = parseInt(inputKM.value);
         console.log("valoreKM: ", valoreKM);
 
+        //moltiplico userKM a 0.21
+        const prezzoKM = valoreKM * 0.21;
+
+        //calcolo prezzo biglietto
+        if (valoreAge <= 17) {  //se età è utente è <= a 17 sottraggo prezzoKM al 20%
+            risultato = prezzoKM - (prezzoKM * 0.2);
+        } else if (valoreAge >= 65) {  	//altrimenti se età è utente è >= a 65 sottraggo prezzoKM al 40%
+            risultato = prezzoKM - (prezzoKM * 0.4);  
+        } else {   //altimenti il risultato è uguale al prezzo al km
+            risultato = prezzoKM;
+        }
+        console.log(risultato);
     }
 );
 
